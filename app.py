070149f8c46ceb7b053cb396d89448c31af8209a
Goshin0831 <63432325+Goshin0831@users.noreply.github.com>
2021-03-02 22:09:44 +0900

    細部の手直し

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from Flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template, redirect, url_for
 
 app = Flask(__name__)
 
@@ -14,13 +14,13 @@ class UserInfo:
 
 #データベースは仮処置とする
 member_list = [
-    UserInfo(0, 'taro', '男', 'Webデザイン', image/taro.jpg),
-    UserInfo(1, 'gen', '男', 'コンサル', image/gen.jpg),
-    UserInfo(2, 'akiko', '女', 'コーダー', image/akiko.jpg),
-    UserInfo(3, 'hanako', '女', '経理', image/hanako.jpg),
-    UserInfo(4, 'satoshi', '男', 'SE', image/satoshi.jpg),
-    UserInfo(5, 'keiko', '女', 'データサイエンティスト', image/keiko.jpg),
-    UserInfo(6, 'kenichi', '男', 'SE', image/kenichi.jpg)
+    UserInfo(0, 'taro', 23, '男', 'Webデザイン', 'image/taro.jpg'),
+    UserInfo(1, 'gen', 32, '男', 'コンサル', 'image/gen.jpg'),
+    UserInfo(2, 'akiko', 26, '女', 'コーダー', 'image/akiko.jpg'),
+    UserInfo(3, 'hanako', 18, '女', '経理', 'image/hanako.jpg'),
+    UserInfo(4, 'satoshi', 32, '男', 'SE', 'image/satoshi.jpg'),
+    UserInfo(5, 'keiko', 21, '女', 'データサイエンティスト', 'image/keiko.jpg'),
+    UserInfo(6, 'kenichi', 22, '男', 'SE', 'image/kenichi.jpg')
 ]
 
 @app.route('/') #mainPage
@@ -32,14 +32,15 @@ def load_member_list():
     return render_template('member_list.html', member_list = member_list)
 
 @app.route('/member/<int:member_number>') #Introduction of membersPage
-def member_ditail(member_number):
+def member_detail(member_number):
     for member in member_list:
         if member.number == member_number:
-            return render_template('member_ditail.html', member = member)
+            return render_template('member_detail.html', member = member)
     return redirect(url_for('main'))
 
 @app.route('/terms') #利用規約
-def terms render_template('terms.html')
+def terms_of_service():
+    return render_template('terms.html')
 
 @app.errorhandler(404) #間違ったページに飛ぶと'main’に戻される
 def redirect_main_page(error):
